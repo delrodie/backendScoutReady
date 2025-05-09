66d06f5b70cbd8a706eb29d177982c037fe87d1d
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2025-05-09 16:09:05 +0000

    Dashboard page

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,23 @@
 import './bootstrap.js';
+import $ from 'jquery';
+window.$ = window.jQuery = $;
+import 'bootstrap';
+import './menu.js';
+
+$(document).ready(function() {
+    console.log("jQuery est prêt !");
+});
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Ici ces't le log");
+});
+
 /*
  * Welcome to your app's main JavaScript file!
  *
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/app.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
---
+++ assets/menu.js
@@ -0,0 +1,23 @@
+    document.addEventListener("DOMContentLoaded", function () {
+        const menuToggle = document.querySelector(".brand-bar");
+        const menuVertical = document.querySelector(".menu-vertical");
+        const mainContent = document.getElementById("mainContent");
+
+        menuToggle.addEventListener("click", function (e) {
+            e.preventDefault();
+            if (window.innerWidth < 992) {
+                menuVertical.classList.toggle("menu-visible");
+            } else {
+                menuVertical.classList.toggle("menu-hidden");
+                mainContent.classList.toggle("expanded");
+            }
+        });
+
+        const closeBtn = document.querySelector(".close-menu");
+        if (closeBtn) {
+            closeBtn.addEventListener("click", function (e) {
+                e.preventDefault();
+                menuVertical.classList.remove("menu-visible");
+            });
+        }
+    });
